8bc2c2af19930b01cf8c262726bf6b106163c671
pei <290633034@qq.com>
2021-02-25 14:34:44 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,9 @@
+# todo: 
+# -*- coding: utf-8 -*-
+"""
+@Project: gitLearn
+@Creator: tester_pei
+@Create Time: 2021-02-25 14:04
+@IDE: PyCharm
+"""
+print('test1')
---
+++ test2.py
@@ -0,0 +1,10 @@
+# todo: 
+# -*- coding: utf-8 -*-
+"""
+@Project: gitLearn
+@Creator: tester_pei
+@Create Time: 2021-02-25 14:04
+@IDE: PyCharm
+"""
+print('test2')
+token = '95a581952752f470f66affe1ec4e6078a6358208'
